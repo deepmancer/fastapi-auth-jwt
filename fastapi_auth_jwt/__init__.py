c580ed27fa47b24a3dc8639cf2abeed41187d3c7
alirezaheidarics <alireza_heidari@sfu.ca>
2024-08-13 22:06:55 +0330

    imported main apis

--- fastapi_auth_jwt/__init__.py
+++ fastapi_auth_jwt/__init__.py
@@ -1,2 +1,9 @@
-__version__ = "1.0.0"
+__version__ = "0.1.0"
 __url__ = "https://github.com/deepmancer/fastapi-auth-jwt"
+
+from .backend import JWTAuthBackend
+from .config.auth_token import AuthConfig
+from .config.storage import RedisConfig, StorageConfig
+from .config.storage_type import StorageTypes
+from .config.user_schema import User
+from .middleware import JWTAuthenticationMiddleware
